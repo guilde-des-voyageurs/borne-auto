cf4385e6a40346d2a618bb4d64c367d377a8a0e5
UrielLahoussaye <uriel@lahoussaye.fr>
2025-01-08 00:08:33 +0100

    Initial commit

---
+++ src/app/utils/shopify.ts
@@ -0,0 +1,19 @@
+export async function getProducts() {
+    try {
+      const response = await fetch(
+        `https://${process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN}/admin/api/2024-01/products.json`,
+        {
+          headers: {
+            'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN!,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+  
+      const products = await response.json();
+      return products;
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      return [];
+    }
+  }
